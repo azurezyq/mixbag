a60d8b6951515e711fc2d917466868aa855fd4b3
Yunqing Zhou <azurezyq@gmail.com>
2026-03-02 22:51:27 -0800

    Fix Android refresh: register SW in index.ejs, add auto-reload on SW update

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mixbag-pwa-v5';
+const CACHE_NAME = 'mixbag-pwa-v6';
 const urlsToCache = [
     '/',
     '/manifest.json',
@@ -22,7 +22,12 @@ self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys =>
             Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-        ).then(() => self.clients.claim()) // Take control of all open tabs
+        ).then(() => self.clients.claim()).then(() => {
+            // Notify all open clients that a new SW has taken over
+            self.clients.matchAll({ includeUncontrolled: true }).then(clients => {
+                clients.forEach(client => client.postMessage({ type: 'SW_UPDATED' }));
+            });
+        })
     );
 });
 
